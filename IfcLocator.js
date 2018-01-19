cd4b9bfbecf1c78b2e6365f5dcdda6678ee4bd4a
Abdou <diakite.abdoulaye1@gmail.com>
2018-01-19 11:10:51 +0100

    Can load, relocate, and export new file with new location now

--- IfcLocator.js
+++ IfcLocator.js
@@ -4,14 +4,19 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 /// Global warnings and variables
 ////////////////////////////////////////////////////////////////////////////////////////////
 var LatLongElev, box, oriLongLatElev = [-1000.0, -1000.0, -1000.0];
-var newLat, newLong;
+var lat_str, long_str, elev_str, myfileLineByLine, idx_IFCSITELine, IFCSITELine;
+var newLat = -1000.0, newLong = -1000.0;
 
+var myfile, mytext;
 
 // Check for the various File API support.
-if (window.File && window.FileReader && window.FileList && window.Blob) {
+if (window.File && window.FileReader && window.FileList && window.Blob) 
+{
   // Great success! All the File APIs are supported.
 //alert('Ales Goed!!!');
-} else {
+} 
+else 
+{
   alert('Your browser does not seem to support our file reader (HTML5 File APIs).');
 }
 
@@ -39,7 +44,8 @@ function FlyToMyModel()
 var handler;
 var scene = viewer.scene;
 
-var entity = viewer.entities.add({
+var pick_label_entity = viewer.entities.add({
+        id : "location_label",
         label : {
             show : false,
             showBackground : true,
@@ -54,6 +60,7 @@ var entity = viewer.entities.add({
 // Mouse over the globe to see the cartographic position
 handler = new Cesium.ScreenSpaceEventHandler(scene.canvas);
 handler.setInputAction(function(movement) {
+
     var cartesian = viewer.camera.pickEllipsoid(movement.endPosition, scene.globe.ellipsoid);
     if (cartesian) 
     {
@@ -63,21 +70,21 @@ handler.setInputAction(function(movement) {
         var longitudeString = Cesium.Math.toDegrees(cartographic.longitude);
         var latitudeString = Cesium.Math.toDegrees(cartographic.latitude);
 
-        entity.position = cartesian;
-        entity.label.show = true;
-        // entity.label.text =
+        pick_label_entity.position = cartesian;
+        pick_label_entity.label.show = true;
+        // pick_label_entity.label.text =
         //     'Lon: ' + ('   ' + longitudeString).slice(-7) + '\u00B0' +
         //     '\nLat: ' + ('   ' + latitudeString).slice(-7) + '\u00B0';
-        entity.label.text =
+        pick_label_entity.label.text =
             'Lon: ' + ('   ' + longitudeString) +
             '\nLat: ' + ('   ' + latitudeString);
 
         newLat = latitudeString;
         newLong = longitudeString;
 
-        console.log(entity.label.text);
+        console.log(pick_label_entity.label.text);
     } else {
-        entity.label.show = false;
+        pick_label_entity.label.show = false;
     }
 }, Cesium.ScreenSpaceEventType.MOUSE_MOVE, Cesium.KeyboardEventModifier.SHIFT);
 
@@ -116,21 +123,26 @@ function GetBoxCoord(LongLatElev, bbox2d)
 ////////////////////////////////////////////////////////////////////////////////////////////
 function ChangeModelLocation()
 {
-  if (oriLongLatElev[0] != -1000 && oriLongLatElev[1] != -1000)
+  if (newLong != -1000 && newLat != -1000)
   {
     oriLongLatElev[0] = newLong; 
     oriLongLatElev[1] = newLat;
-    box = GetBoxCoord([newLong, newLat, 0.0], [-25,25,-25,25]);
+    box = GetBoxCoord([newLong, newLat, elev_str], [-25,25,-25,25]);
+
+    console.log("newLat = " + newLat);
+    console.log("newLong = " + newLong);
+    console.log("elev_str = " + elev_str);
+
+    viewer.entities.removeById("myIFCbox");
 
     // Couldn't find a way to modify existing entity...
-    viewer.entities.removeById("myIFCbox");
-    viewer.entities.add(
+    myIFCbox = viewer.entities.add(
     {
       id : "myIFCbox",
       polygon : 
       {
           hierarchy : new Cesium.PolygonHierarchy(Cesium.Cartesian3.fromDegreesArray( box ) ),
-          height : 0.0,
+          height : elev_str,
           extrudedHeight : 10.0,
           outline : true,
           outlineColor : Cesium.Color.WHITE,
@@ -140,53 +152,118 @@ function ChangeModelLocation()
     });
 
     FlyToMyModel();
+    writeNewIFCLocation();
     // viewer.entities.remove(entity);
   }
   else
-    alert('There is no loaded model to move!')
+    alert('There is no loaded model to move or no new location specified!')
 }
 
 
+
 ////////////////////////////////////////////////////////////////////////////////////////////
-/// Function to handle the opened files (IFC in this case)
+/// Function to open and handle the opened files (IFC in this case)
+/// With a progress bar included
+/// Adapted from: https://www.html5rocks.com/en/tutorials/file/dndfiles/
 ////////////////////////////////////////////////////////////////////////////////////////////
+
+var reader;
+var progress = document.querySelector('.percent');
+
+function abortRead() {
+  reader.abort();
+}
+
+function errorHandler(evt) {
+  switch(evt.target.error.code) {
+    case evt.target.error.NOT_FOUND_ERR:
+      alert('File Not Found!');
+      break;
+    case evt.target.error.NOT_READABLE_ERR:
+      alert('File is not readable');
+      break;
+    case evt.target.error.ABORT_ERR:
+      break; // noop
+    default:
+      alert('An error occurred reading this file.');
+  };
+}
+
+function updateProgress(evt) {
+  // evt is an ProgressEvent.
+  if (evt.lengthComputable) {
+    var percentLoaded = Math.round((evt.loaded / evt.total) * 100);
+    // Increase the progress bar length.
+    if (percentLoaded < 100) {
+      progress.style.width = percentLoaded + '%';
+      progress.textContent = percentLoaded + '%';
+    }
+  }
+}
+
 function handleFileSelect(evt) 
 {
-    var files = evt.target.files; // FileList object
 
-//Retrieve the first (and only!) File from the FileList object
-    var f = evt.target.files[0]; 
+// Reset progress indicator on new file selection.
+  progress.style.width = '0%';
+  progress.textContent = '0%';
 
-    if (f) 
-    {
-      var r = new FileReader();
-      r.onload = function(e) 
-      { 
-        var contents = e.target.result;
+  reader = new FileReader();
+  reader.onerror = errorHandler;
+  reader.onprogress = updateProgress;
+  reader.onabort = function(e) {
+    alert('File read cancelled');
+  };
+  reader.onloadstart = function(e) {
+    document.getElementById('progress_bar').className = 'loading';
+  };
+  reader.onload = function(e) {
+    // Ensure that the progress bar displays 100% at the end.
+    progress.style.width = '100%';
+    progress.textContent = '100%';
+    setTimeout("document.getElementById('progress_bar').className='';", 2000);
+  }
+
+  // Get the first file and its extension
+  myfile = evt.target.files[0];
+  reader.readAsBinaryString(myfile);
+  // Get the extension of the file
+  var ext = myfile.name.split('.').pop();
+
+  if (myfile && ext == "ifc") 
+  {
+    var r = new FileReader();
+    r.onload = function(e) 
+    { 
+      var contents = e.target.result;
+      if (GetLatLongElev_from_IFC(contents))
+      {
         alert( "Got the file.n" 
-              +"name: " + f.name + "\n"
-              +"type: " + f.type + "\n"
-              +"size: " + f.size + " bytes\n"
-              + "starts with: \nLat = " + GetLatLongElev_from_IFC(contents)[0]
-              + "\nLong = " + GetLatLongElev_from_IFC(contents)[1]
-              + "\nElev = " + GetLatLongElev_from_IFC(contents)[2]
-              // + "\n " +  GetBoxCoord(GetLatLongElev_from_IFC(contents), [-5,5,-5,5])
-        );
-
-        LatLongElev = GetLatLongElev_from_IFC(contents);
-        box = GetBoxCoord(LatLongElev, [-25,25,-25,25]);
+                +"name: " + myfile.name + "\n"
+                +"type: " + myfile.type + "\n"
+                +"size: " + myfile.size + " bytes\n"
+                + "starts with: \nLat = " + oriLongLatElev[0]
+                + "\nLong = " + oriLongLatElev[1]
+                + "\nElev = " + oriLongLatElev[2]
+                // + "\n " +  GetBoxCoord(GetLatLongElev_from_IFC(contents), [-5,5,-5,5])
+          );
+    
+        box = GetBoxCoord(oriLongLatElev, [-25,25,-25,25]);
         
-        console.log("Value of LatLongElev: \n" + LatLongElev);
+        // console.log("Value of LatLongElev: \n" + oriLongLatElev);
         console.log("my box: " + box);
         console.log("center: " + oriLongLatElev);
-
+  
+  
+        viewer.entities.removeById("myIFCbox");
+        pick_label_entity.label.show = false;
         viewer.entities.add(
         {
           id : "myIFCbox",
           polygon : 
           {
               hierarchy : new Cesium.PolygonHierarchy(Cesium.Cartesian3.fromDegreesArray( box ) ),
-              height : 0.0,
+              height : elev_str,
               extrudedHeight : 10.0,
               outline : true,
               outlineColor : Cesium.Color.WHITE,
@@ -194,17 +271,21 @@ function handleFileSelect(evt)
               material : Cesium.Color.fromRandom({alpha : 0.5})
             }
         });
-
+  
         FlyToMyModel();
-
       }
-      r.readAsText(f);
-      
+
+      else
+        alert("No geolocation information found in the model.");
+
     }
-    else 
-    { 
-      alert("Failed to load file");
-    }
+    r.readAsText(myfile);
+    
+  }
+  else 
+  { 
+    alert("Failed to load the file (probably not a -valid- IFC)...");
+  }
 
   //   // files is a FileList of File objects. List some properties.
   //   var output = [];
@@ -221,56 +302,171 @@ function handleFileSelect(evt)
 document.getElementById('files').addEventListener('change', handleFileSelect, false);
 
 
+function GetIFCFileContent(ifcfile)
+{
+  // Split the file in lines by relying on the ";"
+  myfileLineByLine = ifcfile.split(";");
+
+  // Find the index of the IFCSITE line
+  idx_IFCSITELine = myfileLineByLine.findIndex(
+      function findIFCSITE(element) 
+      {return element.includes("IFCSITE");}
+    );
+
+  // Get the line
+  IFCSITELine = myfileLineByLine[idx_IFCSITELine];
+
+  // Split it to extract the Lat/Long/Elevation information
+  var splitIFCSITELine = IFCSITELine.split("(");
+
+  if (splitIFCSITELine.length < 4)
+    return false;
+  else
+  {
+    var lat_str_ori = splitIFCSITELine[2].split(")"), 
+        long_str_ori = splitIFCSITELine[3].split(")"), 
+        elev_ori = long_str_ori[1].split(",");
+  
+    console.log("IFCSITELine = " + IFCSITELine);
+    console.log("lat_str_ori = " + lat_str_ori);
+    console.log("long_str_ori = " + long_str_ori);
+    console.log("elev_ori = " + elev_ori);
+  
+    lat_str = lat_str_ori[0].split(",");
+    long_str = long_str_ori[0].split(",");
+    
+    // If the elevation is not defined put it to 0
+    if (elev_ori[1] !== "$")
+      elev_str = elev_ori[1];
+    else
+      elev_str = 0.0;
+    // elev_str = 5.0;
+  
+    console.log("lat_str = " + lat_str);
+    console.log("long_str = " + long_str);
+    console.log("elev_str = " + elev_str);
+
+    return true;
+  }
+}
+
+function writeNewIFCLocation()
+{
+  // Split it to extract the Lat/Long/Elevation information
+  var newIFCSITELine = IFCSITELine.split("(");
+  var newLatSexa = ToSexagesimal(newLat), 
+      newLongSexa = ToSexagesimal(newLong);
+
+  newIFCSITELine[2] = newLatSexa + "),";
+  newIFCSITELine[3] = newLongSexa + ")" + newIFCSITELine[3].split(")")[1] + ")";
+
+  mytext = myfileLineByLine;
+  mytext[idx_IFCSITELine] = newIFCSITELine.join('(');
+
+
+  console.log("\noldIFCSITELine = " + IFCSITELine);
+  console.log("\nnewIFCSITELine = " + newIFCSITELine.join('(') );
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+/// Converts decimal coordinates to sexagesimal (Degree Min Sec Millisec)
+////////////////////////////////////////////////////////////////////////////////////////////
+function ToSexagesimal(CoordInDecimal)
+{
+  var splitCoord, deg, 
+      min, splitmin, 
+      sec, splitsec, 
+      milli;
+
+  splitCoord = CoordInDecimal.toString().split(".");
+  
+  deg = splitCoord[0];
+  min = ("0." + splitCoord[1]) * 60.0;
+  if (min)
+  {
+    splitmin = min.toString().split(".");
+    min = splitmin[0];
+    sec = ("0." + splitmin[1]) * 60.0;
+    if (sec)
+    {
+      splitsec = sec.toString().split(".");
+      sec = splitsec[0];
+      milli = splitsec[1];
+    }
+    else
+      sec = 0.0;
+  }
+  else
+  {
+    min = 0.0;
+    sec = 0.0;
+  }
+
+  // Put negative values if required
+  if (deg < 0)
+  {
+    min = min * -1.0;
+    sec = sec * -1.0;
+    milli = milli * -1.0;
+  }
+
+  // limit the size of the output to 6 digits
+  if (milli)
+  {
+    if (milli.toString().length < 6)
+      milli = (milli.toString().padEnd(6, "0")) * (deg/Math.abs(deg));
+    else if (milli.toString().length > 6)
+      milli = (milli.toString().substr(0, 6)) * (deg/Math.abs(deg));
+
+    console.log("\nSexagesimal Coordinates: " + deg + ", " + min + ", " + sec + ", " + milli ); 
+    return [deg, min, sec, milli];
+  }
+  else
+    {
+      console.log("\nSexagesimal Coordinates: " + deg + ", " + min + ", " + sec + ", " + milli );
+      return [deg, min, sec];
+    }
+
+}
+
 ////////////////////////////////////////////////////////////////////////////////////////////
 /// Parse the IFC file and get the Lat/Long/Elevation data
 ////////////////////////////////////////////////////////////////////////////////////////////
 function GetLatLongElev_from_IFC(ifcfile)
 {
-  // Locates IFCSITE line and return a string segment of lenght 300 containing it
-  var getIFCSITE = ifcfile.substr(ifcfile.search("IFCSITE"), 300), 
-      getArrayOfLines = getIFCSITE.split(";"), 
-      getIFCSITELine = getArrayOfLines[0];
 
-  var splitIFCSITELine = getIFCSITELine.split("("), 
-      lat_str_ori = splitIFCSITELine[2].split(")"), 
-      long_str_ori = splitIFCSITELine[3].split(")"), 
-      elev_ori = long_str_ori[1].split(",");
-
-  console.log("lat_str_ori = " + lat_str_ori);
-  console.log("long_str_ori = " + long_str_ori);
-  console.log("elev_ori = " + elev_ori);
-  
-  var lat_str = lat_str_ori[0].split(","), 
-      long_str = long_str_ori[0].split(","), 
-      elev_str = elev_ori[1];
-
-  console.log("lat_str = " + lat_str);
-  console.log("long_str = " + long_str);
-  console.log("elev_str = " + elev_str);
-
-  var lat = 0, long = 0;
-  
-  if (lat_str.length == 4)
-    lat_str[2] += "." + Math.abs(lat_str[3]);
-  for (i = 0; i<3; i++)
+  if ( GetIFCFileContent(ifcfile) )
   {
-    lat += lat_str[i]/Math.pow(60,i);
-    // console.log(lat);
-    console.log( "For i = " + i + ", lat = " + lat_str[i] + " / " + Math.pow(60,i) + " = " + lat);
+    var lat = 0, long = 0;
+    if (lat_str.length == 4)
+      lat_str[2] += "." + Math.abs(lat_str[3]);
+    for (i = 0; i<3; i++)
+    {
+      lat += lat_str[i]/Math.pow(60,i);
+      // console.log(lat);
+      // console.log( "For i = " + i + ", lat = " + lat_str[i] + " / " + Math.pow(60,i) + " = " + lat);
+    }
+  
+    // console.log(long_str.length);
+    if (long_str.length == 4)
+      long_str[2] += "." + Math.abs(long_str[3]);
+    for (i = 0; i<3; i++)
+    {
+      long += long_str[i]/Math.pow(60,i);
+      // console.log( "For i = " + i + ", long = " + long_str[i] + " / " + Math.pow(60,i) + " = " + long);
+    }
+  
+    ToSexagesimal(long);
+    ToSexagesimal(lat);
+  
+    // IFC provides Lat-Long while Cesium reads Long-Lat
+    oriLongLatElev = [long, lat, elev_str];
+    return true;
   }
+  else
+    return false;
 
-  // console.log(long_str.length);
-  if (long_str.length == 4)
-    long_str[2] += "." + Math.abs(long_str[3]);
-  for (i = 0; i<3; i++)
-  {
-    long += long_str[i]/Math.pow(60,i);
-    console.log( "For i = " + i + ", long = " + long_str[i] + " / " + Math.pow(60,i) + " = " + long);
-  }
-
-  // IFC provides Lat-Long while Cesium reads Long-Lat
-  oriLongLatElev = [long, lat, elev_str];
-  return oriLongLatElev;
 };
 
 // viewer.entities.add({
@@ -287,3 +483,18 @@ function GetLatLongElev_from_IFC(ifcfile)
 //         material : Cesium.Color.fromRandom({alpha : 1.0})
 //     }
 // });
+
+
+function ExportLocation()
+{
+  if (newLong != -1000 && newLat != -1000 && mytext)
+  {
+    var text = mytext.join(';');
+    var filename = "Relocated_" + myfile.name;
+    var blob = new Blob([text], {type: "text/plain;charset=utf-8"});
+    saveAs(blob, filename);
+  }
+  else
+    alert('There is no new location specified!')
+
+}
